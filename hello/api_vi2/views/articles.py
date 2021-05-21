07e47f1c35c505379c0c8916bd317a0eac7a75b3
alishka1216 <alishka1216@gmail.com>
2021-05-21 20:31:43 +0600

    создал view для API django generic

--- hello/api_vi2/views/articles.py
+++ hello/api_vi2/views/articles.py
@@ -1,7 +1,10 @@
 import json
 
+from django.db.migrations import serializer
 from django.http import JsonResponse
 from django.views import View
+from rest_framework import status
+from rest_framework.generics import UpdateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -12,16 +15,44 @@ from api_vi2.serializers import ArticleSerializer, article
 
 
 class ArticleView(APIView):
-    def get(self, request, *args, **kwargs):
+    def get(self, request,  *args, **kwargs):
+        pk = kwargs.get('pk')
+        try:
+            if pk:
+                return Response(ArticleSerializer(Article.objects.get(pk=pk)).data)
+        except Article.DoesNotExist as e:
+            return Response({'error': "error"}, status=status.HTTP_400_BAD_REQUEST)
         articles = Article.objects.all()
+        a = ArticleSerializer(articles, many=True)
+        print(a)
+        print()
+        print(a.data)
         response_data = ArticleSerializer(articles, many=True).data
-        print(response_data)
         return Response(data=response_data)
 
 
     def post(self, request, *args, **kwargs):
         article_data = request.data
         serializer = ArticleSerializer(data={**article_data, 'qwe':''})
-        serializer.is_valid(raise_exception=True)
-        article = serializer.save()
-        return JsonResponse({'id': article.id})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk, *args, **kwargs):
+        article_data = request.data
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=article_data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk):
+        try:
+            article = Article.objects.get(pk=pk)
+            article.delete()
+        except Article.DoesNotExist as e:
+            return Response({'error': "error"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"pk": pk})
