34fee5a2dfe5aac1f65eacfeb4e94e98cb9f6a64
alishka1216 <alishka1216@gmail.com>
2021-05-26 18:27:29 +0600

    домашка

--- hello/api_vi2/views/articles.py
+++ hello/api_vi2/views/articles.py
@@ -35,53 +35,53 @@ class ArticleViewSet(ModelViewSet):
 
 
 
-# class ArticleListView(generics.ListAPIView):
-#     queryset = Article.objects.all()
-#     serializer_class = ArticleSerializer
-#
-# class ArticleCreateView(generics.CreateAPIView):
-#     queryset = Article.objects.all()
-#     serializer_class = ArticleSerializer
-#
+class ArticleListView(generics.ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
-    # def get(self, request,  *args, **kwargs):
-    #     pk = kwargs.get('pk')
-    #     try:
-    #         if pk:
-    #             return Response(ArticleSerializer(Article.objects.get(pk=pk)).data)
-    #     except Article.DoesNotExist as e:
-    #         return Response({'error': "error"}, status=status.HTTP_400_BAD_REQUEST)
-    #     articles = Article.objects.all()
-    #     a = ArticleSerializer(articles, many=True)
-    #     print(a)
-    #     print()
-    #     print(a.data)
-    #     response_data = ArticleSerializer(articles, many=True).data
-    #     return Response(data=response_data)
-    #
-    #
-    # def post(self, request, *args, **kwargs):
-    #     article_data = request.data
-    #     serializer = ArticleSerializer(data={**article_data, 'qwe':''})
-    #     if serializer.is_valid(raise_exception=True):
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    #
-    # def put(self, request, pk, *args, **kwargs):
-    #     article_data = request.data
-    #     article = Article.objects.get(pk=pk)
-    #     serializer = ArticleSerializer(article, data=article_data)
-    #     if serializer.is_valid(raise_exception=True):
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    #
-    #
-    # def delete(self, request, pk):
-    #     try:
-    #         article = Article.objects.get(pk=pk)
-    #         article.delete()
-    #     except Article.DoesNotExist as e:
-    #         return Response({'error': "error"}, status=status.HTTP_400_BAD_REQUEST)
-    #     return Response({"pk": pk})
+class ArticleCreateView(generics.CreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+    def get(self, request,  *args, **kwargs):
+        pk = kwargs.get('pk')
+        try:
+            if pk:
+                return Response(ArticleSerializer(Article.objects.get(pk=pk)).data)
+        except Article.DoesNotExist as e:
+            return Response({'error': "error"}, status=status.HTTP_400_BAD_REQUEST)
+        articles = Article.objects.all()
+        a = ArticleSerializer(articles, many=True)
+        print(a)
+        print()
+        print(a.data)
+        response_data = ArticleSerializer(articles, many=True).data
+        return Response(data=response_data)
+
+
+    def post(self, request, *args, **kwargs):
+        article_data = request.data
+        serializer = ArticleSerializer(data={**article_data, 'qwe':''})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk, *args, **kwargs):
+        article_data = request.data
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=article_data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk):
+        try:
+            article = Article.objects.get(pk=pk)
+            article.delete()
+        except Article.DoesNotExist as e:
+            return Response({'error': "error"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"pk": pk})
